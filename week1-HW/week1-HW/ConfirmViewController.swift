335a08bb9b693fe3313f9d894c1060af5ab73cb0
loinsir <a9327370@gmail.com>
2021-10-04 17:29:09 +0900

    💄[MOD] storyboard에서 UI 구현 (#1)

---
+++ week1-HW/week1-HW/ConfirmViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ConfirmViewController.swift
+//  week1-HW
+//
+//  Created by 김인환 on 2021/10/04.
+//
+
+import UIKit
+
+class ConfirmViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
