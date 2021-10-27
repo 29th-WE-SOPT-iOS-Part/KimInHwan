9ff0dab8c7191b8a6f7782427f2c71568c08e995
loinsir <a9327370@gmail.com>
2021-10-27 21:40:07 +0900

    ♻️[[REFACTOR] 뷰컨별로 스토리보드 분할 (#6)

---
+++ 29th-iOS-HW/29th-iOS-HW/TabbarViewController.swift
@@ -0,0 +1,8 @@
+//
+//  TabbarViewController.swift
+//  29th-iOS-HW
+//
+//  Created by 김인환 on 2021/10/27.
+//
+
+import Foundation
---
+++ 29th-iOS-HW/29th-iOS-HW/storyboard/TabbarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TabbarViewController.swift
+//  29th-iOS-HW
+//
+//  Created by 김인환 on 2021/10/27.
+//
+
+import UIKit
+
+class TabbarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
