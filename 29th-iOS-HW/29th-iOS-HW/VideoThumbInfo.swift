a026116cd94674bce32a44ea07f723e556aff609
loinsir <a9327370@gmail.com>
2021-10-28 02:02:44 +0900

    ➕[ADD] 데이터에 유연하게 대처하도록 비디오셀 모델 정의 (#6)

---
+++ 29th-iOS-HW/29th-iOS-HW/VideoThumbInfo.swift
@@ -0,0 +1,15 @@
+//
+//  VideoThumbInfo.swift
+//  29th-iOS-HW
+//
+//  Created by 김인환 on 2021/10/28.
+//
+
+import UIKit
+
+struct VideoThumbInfo {
+    var thumbnailImage: UIImage
+    var userProfileImage: UIImage
+    var title: String
+    var metadataString: String
+}
