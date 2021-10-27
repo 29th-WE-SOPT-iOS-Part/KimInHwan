3abd71e1e0a3b0db4c4a32a864f95d8b5c7aa81d
loinsir <a9327370@gmail.com>
2021-10-28 03:42:26 +0900

    ➕[ADD] (도전과제) 추천 항목 데이터 추가 (#6)

---
+++ 29th-iOS-HW/29th-iOS-HW/RecommendCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  RecommendCollectionViewCell.swift
+//  29th-iOS-HW
+//
+//  Created by 김인환 on 2021/10/28.
+//
+
+import UIKit
+
+class RecommendCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
