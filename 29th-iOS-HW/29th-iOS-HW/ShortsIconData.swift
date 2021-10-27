fdea80acc186c6377d1f72a386e48787789051ac
loinsir <a9327370@gmail.com>
2021-10-28 02:37:12 +0900

    ➕[ADD] 쇼츠 아이콘 데이터 추가 (#6)

---
+++ 29th-iOS-HW/29th-iOS-HW/ShortsIconData.swift
@@ -0,0 +1,19 @@
+//
+//  ShortsData.swift
+//  29th-iOS-HW
+//
+//  Created by 김인환 on 2021/10/28.
+//
+
+import Foundation
+
+struct ShortsIconData {
+    let userList: [String] = [
+        "iOSPart",
+        "AndroidPart",
+        "ServerPart",
+        "WebPart",
+        "DesignPart",
+        "PlanPart"
+    ]
+}
