fccd9e127c0ab4c63dcae0eebd81425eafb66c4d
loinsir <a9327370@gmail.com>
2021-11-10 15:02:39 +0900

    ➕[ADD] 네트워크 모델 추가 (#8)

---
+++ 29th-iOS-HW/29th-iOS-HW/Networking/APIConstants.swift
@@ -0,0 +1,16 @@
+//
+//  APIConstants.swift
+//  29th-iOS-HW
+//
+//  Created by 김인환 on 2021/11/10.
+//
+
+import Foundation
+
+struct APIConstants {
+    //MARK: Base URL
+    static let baseURL: String = "https://asia-northeast3-we-sopt-29.cloudfunctions.net/api"
+    
+    //MARK: Feature URL
+    static let signInURL: String = baseURL + "/user/logins"
+}
