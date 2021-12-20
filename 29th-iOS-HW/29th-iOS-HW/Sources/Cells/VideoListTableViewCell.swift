7da50274109961701f5261c192bde9057b13a91d
asdasd <a9327370@gmail.com>
2021-12-21 02:25:13 +0900

    ✨[FEAT] 재생화면 데이터 전달 구현

--- 29th-iOS-HW/29th-iOS-HW/Sources/Cells/VideoListTableViewCell.swift
+++ 29th-iOS-HW/29th-iOS-HW/Sources/Cells/VideoListTableViewCell.swift
@@ -7,16 +7,37 @@
 
 import UIKit
 
+protocol VideoListTableViewCellDelegate {
+    func didTouchThumbnailImage(cell: VideoListTableViewCell)
+}
+
 class VideoListTableViewCell: UITableViewCell {
     
+    // MARK: - Properties
+    
     static let identifier: String = "VideoListTableViewCell"
+    var delegate: VideoListTableViewCellDelegate?
+    
+    // MARK: - IBOutlet
 
     @IBOutlet weak var thumbnailImage: UIImageView!
     @IBOutlet weak var userProfileImage: UIImageView!
     @IBOutlet weak var videoTitleLabel: UILabel!
     @IBOutlet weak var metadataLabel: UILabel!
     
+    // MARK: - Life Cycle
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        setGestureRecognizer()
+    }
+    
+    func setGestureRecognizer() {
+        let gestureRecognizer: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(touchThumbnailImage(_:)))
+        thumbnailImage.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    @objc func touchThumbnailImage(_ sender: UIGestureRecognizer) {
+        delegate?.didTouchThumbnailImage(cell: self)
     }
 }
